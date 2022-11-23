6e91f8b40900f5a735f47b6f02a279f572544e98
Carolina Vilela <carolinavilela50@gmail.com>
2022-11-23 11:21:06 -0300

    iniciei as definições da classe player

--- T3/main.cpp
+++ T3/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "player.hpp"
 
 using namespace std;
 
--- T3/player.hpp
+++ T3/player.hpp
@@ -1,8 +1,33 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
+#include <string>
+#include "bag.hpp"
+#include "item.hpp"
+
+using namespace std;
 
 class Player{
+  private:
+   int id;
+   string nome;
+   int hp;// tem 100 de vida maxima
+   Bag mochila;
 
+   
+  private:
+   void curarVida(int cura);
+
+  public:
+    Player();
+    ~Player();
+
+   int getId();
+   string getNome();
+
+   void adicionarItem(Item item_encontrado); //coloca o item dentro da mochila
+   void receberDano(int dano);
+   void usarPocaoDeCura();// pergunta quantas poções de vida ele quer usar e as usa (chamando o curarVida)
+   void listarItensDaMochila(); // escreve na tela todos os itens que estão na mochila
 };
 
 #endif
