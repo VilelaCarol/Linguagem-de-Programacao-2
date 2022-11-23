2e3077b3510be273ec94fd89da863d5955760391
Carolina Vilela <carolinavilela50@gmail.com>
2022-11-23 10:57:22 -0300

    fiz o menu principal

--- T3/main.cpp
+++ T3/main.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
+void menuCreatePlayer()
+{
+    cout << "CREATING NEW PLAYER" << endl;
+}
+void menuSelectPlayer()
+{
+    cout << "SELECT A PLAYER" << endl;
+}
 
+int main()
+{
+    while (1)
+    {
+        cout << "WELCOME TO POTION FINDER!!!!!!!" << endl;
+        cout << "1 - CREATE PLAYER" << endl;
+        cout << "2 - SELECT A PLAYER TO MAKE A MOVE" << endl;
+        cout << "3 - EXIT" << endl;
+        cout << "SELECT AN OPTION" << endl;
 
+        string option;
+        cin >> option;
 
+        if (option == "1")
+        {
+            menuCreatePlayer();
+        }
+        else if (option == "2")
+        {
+            menuSelectPlayer();
+        }
+        else if (option == "3")
+        {
+            break;
+        }
+        else
+        {
+            cout << "INVALID OPTION" << endl;
+        }
+    }
     return 0;
 }
